3e9eb2ba1b3a830add2f9b7dec8b0a6849137a8f
Phu Thong Pham <ptpham4@myseneca.ca>
2021-10-05 18:14:01 -0400

    Added support for JSON config file

--- magic_ssg.py
+++ magic_ssg.py
@@ -3,6 +3,7 @@ import os
 import shutil
 import re
 import sys
+import json
 
 DIST_FOLDER = "dist"
 
@@ -168,12 +169,31 @@ def main():
     parser.add_argument("-v", "--version", action="version",
                         version="%(prog)s 0.1", help="display tool name and version.")
     parser.add_argument(
-        "-i", "--input", help="specify an input file or folder to be processed.", required=True)
+        "-i", "--input", help="specify an input file or folder to be processed.")
     parser.add_argument('-l', '--lang', nargs='?', type=str, default='en=CA',
                         help='Language of the document (default is en=CA)')
     parser.add_argument("-c", "--config", help="specify a config file to be processed")
     args = parser.parse_args()
     input = args.input
+
+    if args.config is not None:
+        try:
+            with open(args.config, "r", encoding="utf-8") as config_file:
+                config = json.load(config_file)
+                if "input" in config:
+                    input = config["input"]
+                else:
+                    input = None
+                if "lang" in config:
+                    args.lang = config["lang"]
+        except FileNotFoundError:
+            print(f'Error: File "{args.config}" cannot be found!')
+        except json.JSONDecodeError:
+            print(f'Error: File "{args.config} has invalid JSON syntax!" ')
+
+    if input is None:
+        print(f'Error: Input is not found! Please specify a file/folder to be processed.')
+        exit()
 
     if (args.lang is not None):
         langlist = "".join(args.lang)
