c0062937bb99b1c6ee0317da213e005adcb64231
Tengzhen <tengzhenzhao@gmail.com>
2021-11-26 12:47:09 -0500

    change test_get_md_title.py file

--- tests/test_get_md_title.py
+++ tests/test_get_md_title.py
@@ -1,27 +1,6 @@
 from magic_ssg.asstLib import get_md_title
 
 
-# def get_md_title(file_path):
-#     i = 0
-#     title = ""
-#     # Read top 3 lines one by one.
-#     with open(file_path, "r", encoding="utf8") as input_file:
-#         for i in range(3):
-#             for line in input_file.readlines():
-#                 i += 1
-#                 title = line.strip()
-#                 if i == 3:
-#                     break
-#                 elif not len(title):
-#                     continue
-#                 elif (
-#                     title.startswith("#")
-#                     or title.startswith("##")
-#                     or title.startswith("###")
-#                 ):
-#                     return title
-
-
 # test the title with one pound key
 def test_md_title_with_one_pound():
     file_path = "./tests/testfiles/test1.md"
